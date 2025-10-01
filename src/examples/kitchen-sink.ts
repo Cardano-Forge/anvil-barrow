948e0da2c0c050e25913ea93304447cd21a6d9df
Dodilanne <dodilanne@ada-anvil.io>
2025-10-01 17:38:34 -0400

    Cleanup kitchen sink

--- src/examples/kitchen-sink.ts
+++ src/examples/kitchen-sink.ts
@@ -1,7 +1,7 @@
 import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
 import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 import { NodeSDK } from "@opentelemetry/sdk-node";
-import { pino } from "pino";
+import { type Level, pino } from "pino";
 import { assert, unwrap } from "trynot";
 import { Controller } from "../controller";
 import { type OgmiosSchema, OgmiosSyncClient } from "../dep/ogmios";
@@ -9,72 +9,99 @@ import { otelTracingConfig } from "../dep/otel";
 import { pinoLogger } from "../dep/pino";
 import { ErrorHandler } from "../error-handler";
 import { ProcessingError, SocketClosedError, SocketError } from "../errors";
-import { noop } from "../lib/noop";
 
+// Setup ogmios sync client
+const syncClient = new OgmiosSyncClient({
+  host: process.env.OGMIOS_NODE_HOST,
+  port: Number(process.env.OGMIOS_NODE_PORT),
+  tls: Boolean(process.env.OGMIOS_NODE_TLS),
+});
+
+// Setup otel tracing
 new NodeSDK({
   metricReader: new PeriodicExportingMetricReader({
     exporter: new OTLPMetricExporter(),
   }),
 }).start();
+const tracingConfig = otelTracingConfig();
 
-const logger = pino({
-  level: "trace",
-  transport: {
-    targets: [
-      {
-        target: "pino-pretty",
-        options: { colorize: true },
-        level: "trace",
-      },
-      {
-        target: "pino-opentelemetry-transport",
-        level: "trace",
-      },
-    ],
-  },
-});
+// Setup pino logger
+const level: Level = "trace";
+const logger = pinoLogger<OgmiosSchema>(
+  pino({
+    level,
+    transport: {
+      targets: [
+        {
+          level,
+          target: "pino-pretty",
+          options: { colorize: true },
+        },
+        {
+          level,
+          target: "pino-opentelemetry-transport",
+        },
+      ],
+    },
+  }),
+);
+
+// Setup error handling
+const errorHandler = new ErrorHandler()
+  .register(
+    ProcessingError,
+    ErrorHandler.retry({ maxRetries: 1, baseDelay: 5000, persistent: true }),
+  )
+  .register(
+    SocketError,
+    ErrorHandler.retry({ maxRetries: 2, baseDelay: 5000, backoff: true }),
+  )
+  .register(
+    SocketClosedError,
+    ErrorHandler.retry({ maxRetries: 2, baseDelay: 5000, backoff: true }),
+  );
 
 const controller = new Controller({
-  syncClient: new OgmiosSyncClient({
-    host: process.env.OGMIOS_NODE_HOST,
-    port: Number(process.env.OGMIOS_NODE_PORT),
-    tls: Boolean(process.env.OGMIOS_NODE_TLS),
-  }),
-  errorHandler: new ErrorHandler()
-    .register(
-      ProcessingError,
-      ErrorHandler.retry({ maxRetries: 1, baseDelay: 5000, persistent: true }),
-    )
-    .register(
-      SocketError,
-      ErrorHandler.retry({ maxRetries: 2, baseDelay: 5000, backoff: true }),
-    )
-    .register(
-      SocketClosedError,
-      ErrorHandler.retry({ maxRetries: 2, baseDelay: 5000, backoff: true }),
-    ),
-  logger: pinoLogger(logger),
-  tracingConfig: otelTracingConfig(),
+  syncClient,
+  errorHandler,
+  logger,
+  tracingConfig,
 });
 
-const point: OgmiosSchema["pointOrOrigin"] = {
-  id: "fa5a6a51632b90557665fcb33970f4fb372dff6ad0191e083ff3b6b221f2b87e",
-  slot: 101163751,
-};
-
 async function main() {
-  await unwrap(
+  // Start sync job
+  const result = await unwrap(
     controller.start({
-      fn: noop,
-      point,
+      // Throttle event arrival rate
       throttle: [100, "milliseconds"],
-      // Uncomment to run a specific block
-      // filter: (event) => event.type === "apply" && event.block.height === 3859660,
-      // Uncomment to stop after n block(s) are applied
-      // takeUntil: ({ state }) => state.counters.applyCount >= 10,
+
+      // Only process a specific event
+      filter: (event) => {
+        return event.type === "apply" && event.block.height === 3859660;
+      },
+
+      // Complete sync job when event is processed
+      takeUntil: ({ state }) => {
+        return state.counters.applyCount >= 1;
+      },
+
+      fn: (_syncEvent) => {
+        // Process the sync event
+      },
+
+      // Define the starting point
+      point: {
+        id: "fa5a6a51632b90557665fcb33970f4fb372dff6ad0191e083ff3b6b221f2b87e",
+        slot: 101163751,
+      },
     }),
   );
+
+  assert(result.status === "running");
+
+  // Wait for sync job to complete
   await controller.waitForCompletion();
+
   assert(controller.state.status === "done");
 }
 
